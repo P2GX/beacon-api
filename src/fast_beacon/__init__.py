f3409c8f968bb7bf5ff11090845411c7a01c07e2
iimpulse <Michael.Gargano@jax.org>
2025-10-09 09:40:11 -0600

    Update package docstring to reflect fast-beacon branding

--- src/fast_beacon/__init__.py
+++ src/fast_beacon/__init__.py
@@ -1,3 +1,3 @@
-"""Beacon Skeleton - A FastAPI implementation of the GA4GH Beacon v2 API."""
+"""Fast Beacon - A fast, modern FastAPI implementation of the GA4GH Beacon v2 API."""
 
 __version__ = "0.1.0"
